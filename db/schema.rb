d237d06ec2b3737f86c04c3a69e744bd9a59fed5
learning2code123 <lmattos@chicagobooth.edu>
2022-05-26 19:39:11 +0000

    photo draft

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_192223) do
+ActiveRecord::Schema.define(version: 2022_05_26_193830) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.text "caption"
+    t.integer "owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
