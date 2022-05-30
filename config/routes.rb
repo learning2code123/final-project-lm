8b561fb01e5f3aa574e6c95d129798156d530afd
learning2code123 <lmattos@chicagobooth.edu>
2022-05-30 23:20:12 +0000

    garments debug

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
  # Routes for the Looks resource:
 
   # CREATE
-  post("/insert_looks", { :controller => "looks", :action => "create" })
+  post("/insert_looks", { :controller => "photos", :action => "create_look" })
           
   # READ
   get("/looks", { :controller => "looks", :action => "index" })
